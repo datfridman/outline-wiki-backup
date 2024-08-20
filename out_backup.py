a1bfa727dbe5cab37dd71d809eef68b0e33bd87a
Leo Fridman <madfridman@gmail.com>
2024-08-20 16:55:09 +0200

    Update out_backup.py

--- out_backup.py
+++ out_backup.py
@@ -7,7 +7,7 @@ ARCHIVE_NAME="backup_outline_$(date +'%Y%m%d').tar.gz"
 BOT_TOKEN="71111113:AAmdansfuanjn3j12adaPeRyKe_k"  # Your Bot Token from Botfather
 CHAT_ID="-4111111111"      # Telegram chat_id
 
-echo "$(date +'%Y-%m-%d %H:%M:%S') - Starting backup of Outline Wiki" >> $LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Starting backup of Outline Wiki" 
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
     -d chat_id="$CHAT_ID" \
     -d text="Starting backup of Outline  Wiki $(date +'%Y-%m-%d %H:%M:%S')"
@@ -15,7 +15,7 @@ curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
 tar -czf /tmp/$ARCHIVE_NAME -C $SOURCE_FOLDER .
 
 if [ $? -ne 0 ]; then
-  echo "$(date +'%Y-%m-%d %H:%M:%S') - Error while trying to arvhive your data" >> $LOG_FILE
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - Error while trying to arvhive your data" 
   curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
       -d chat_id="$CHAT_ID" \
       -d text="Error while trying to arvhive your data $(date +'%Y-%m-%d %H:%M:%S')"
@@ -41,7 +41,7 @@ done
 rm /tmp/backup_part_*
 rm /tmp/$ARCHIVE_NAME
 
-echo "$(date +'%Y-%m-%d %H:%M:%S') - Backup Done " >> $LOG_FILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') - Backup Done "
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
     -d chat_id="$CHAT_ID" \
     -d text="Backup Done $(date +'%Y-%m-%d %H:%M:%S')"
